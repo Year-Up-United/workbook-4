ae21f851cd10a44aecde97f2b24f49a936b9a989
thejaylajones <janeissaj@gmail.com>
2025-05-05 13:40:04 -0400

    updating before cpu dies

---
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class Reservation {
+
+//    Create the class with the following getters and setters:
+//    getRoomType()
+//    setRoomType(String roomType)
+//    getPrice()
+//    getNumberOfNights()
+//    setNumberOfNights(int numberOfNights)
+//    isWeekend()
+//    setIsWeekend(boolean isWeekend)
+//    getReservationTotal()
+
+}
---
+++ HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class Room {
+
+//    The Room class is responsible for knowing everything related to a hotel room. A
+//    room is only available if it is clean and not currently occupied. Create the class
+//    with the following getters:
+//    getNumberOfBeds()
+//    getPrice()
+//    isOccupied()
+//    isDirty()
+//    isAvailable()
+
+
+
+
+}
