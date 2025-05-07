df91c882ad691f9adeca2f5c08aafb80f0bc5103
thejaylajones <janeissaj@gmail.com>
2025-05-07 16:03:22 -0400

    wed work

--- Blackjack/src/main/java/com/pluralsight/Main.java
+++ Blackjack/src/main/java/com/pluralsight/Main.java
@@ -2,13 +2,28 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        // constructor
+        Deck d = new Deck();
+        d.shuffle();
 
+        // constructor
+        Hand h1 = new Hand();
 
+        // create a single card
+        Card c1 = d.deal();
 
+        // have to flip the card
+        c1.flip();
 
+        // print out something to test
+       System.out.println(c1.getSuit());
+       System.out.println(c1.getValue());
+       System.out.println(c1.getPointValue());
 
+        Card c2 = d,deal();
+        c2.flip();
 
-
-        System.out.println("Hello, World!");
+        h1.deal(c1);
+        h1.deal(c2);
     }
 }
